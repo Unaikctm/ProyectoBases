f62d3a05c7c9dea33ffbc782fd4f1f9b866f7df5
Unai Hernandez <ikctm@plaiaundi.net>
2023-11-23 11:00:16 +0100

    Atributos alumnos añadidos

--- src/clases/Administrador.java
+++ src/clases/Administrador.java
@@ -1,5 +1,55 @@
 package clases;
 
 public class Administrador {
+	
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String username;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Administrador(int id, String nombre, String apellido, String username, String password) {
+		
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.username = username;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Administrador [Id=" + id + ", Nombre=" + nombre + ", Apellido=" + apellido + ", Username=" + username
+				+ ", Password=" + password + "]";
+	}
+	
 
 }
--- src/clases/Alumno.java
+++ src/clases/Alumno.java
@@ -1,5 +1,9 @@
 package clases;
 
 public class Alumno {
-
+	private int id;
+	private String izena;
+	private String apellido;
+	private String grupo;
+	private boolean activo;
 }
