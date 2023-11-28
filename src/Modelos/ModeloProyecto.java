c1f1c71127f7a05c57aef2ec04cd901882de2d41
Unai Hernandez <ikctm@plaiaundi.net>
2023-11-28 09:19:13 +0100

    Primer commit de la branch

---
+++ src/Modelos/ModeloProyecto.java
@@ -0,0 +1,14 @@
+package Modelos;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import clases.Proiektuak;
+import conector.Conector;
+
+public class ModeloProyecto extends Conector{
+	
+	public ArrayList<Proiektuak> selectAll(){
+		
+	}
+}
