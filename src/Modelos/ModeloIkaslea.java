119d110de1550c4b4bac8e7d0a9995d1f98a5078
Eñaut Agirre Arri <enaut.irakasle@gmail.com>
2023-12-03 22:47:39 +0100

    sellectAll, select, insert, delete, update implementados

--- src/Modelos/ModeloIkaslea.java
+++ src/Modelos/ModeloIkaslea.java
@@ -2,12 +2,14 @@ package Modelos;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import clases.Ikaslea;
 import conector.Conector;
 
 public class ModeloIkaslea extends Conector {
+	
 	public ArrayList<Ikaslea> selectAll(){
 		ArrayList<Ikaslea> proiektuak = new ArrayList<Ikaslea>();
 		
@@ -25,6 +27,8 @@ public class ModeloIkaslea extends Conector {
 				
 				proiektuak.add(ikaslea);
 			}
+			rs.close();
+			st.close();
 			return proiektuak;
 		}catch (Exception e) {
 			System.out.println("No se ha podido ejecutar la query");
@@ -33,4 +37,75 @@ public class ModeloIkaslea extends Conector {
 		return null;
 		
 	}
+	
+	public Ikaslea select(int idIkaslea) {
+	    try {
+	        PreparedStatement st = getCon().prepareStatement("SELECT * FROM ikasleak WHERE id = ?");
+	        st.setInt(1, idIkaslea);
+	        ResultSet rs = st.executeQuery();
+
+	        if (rs.next()) {
+	            Ikaslea ikaslea = new Ikaslea();
+	            ikaslea.setId(rs.getInt("id"));
+	            ikaslea.setIzena(rs.getString("izena"));
+	            ikaslea.setAbizena(rs.getString("abizena"));
+	            ikaslea.setTaldea(rs.getString("taldea"));
+	            ikaslea.setAktibo(rs.getBoolean("aktibo_dago"));
+
+	            return ikaslea;
+	        }
+	        rs.close();
+	        st.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+	
+	public void insert(Ikaslea ikaslea) {
+	    try {
+	        PreparedStatement st = getCon().prepareStatement("INSERT INTO ikasleak (izena, abizena, taldea, aktibo_dago) VALUES (?, ?, ?, ?)");
+	        st.setString(1, ikaslea.getIzena());
+	        st.setString(2, ikaslea.getAbizena());
+	        st.setString(3, ikaslea.getTaldea());
+	        st.setBoolean(4, ikaslea.isAktibo());
+
+	        st.executeUpdate();
+	        st.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public void update(Ikaslea ikaslea) {
+	    try {
+	        PreparedStatement st = getCon().prepareStatement("UPDATE ikasleak SET izena = ?, abizena = ?, taldea = ?, aktibo_dago = ? WHERE id = ?");
+	        st.setString(1, ikaslea.getIzena());
+	        st.setString(2, ikaslea.getAbizena());
+	        st.setString(3, ikaslea.getTaldea());
+	        st.setBoolean(4, ikaslea.isAktibo());
+	        st.setInt(5, ikaslea.getId());
+
+	        st.executeUpdate();
+	        st.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public void delete(int idIkaslea) {
+	    try {
+	        PreparedStatement st = getCon().prepareStatement("DELETE FROM ikasleak WHERE id = ?");
+	        st.setInt(1, idIkaslea);
+
+	        st.executeUpdate();
+	        st.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+
+
+
 }
